15ce9b7a2419b207552d398fba1c8cd3f9fdfa17
Reem Khamis <khamisr@codescalers.com>
2015-01-15 11:00:20 +0200

    Pull and update all spaces in portal

--- apps/portalbase/macros/page/pull_update/1_pull_update.py
+++ apps/portalbase/macros/page/pull_update/1_pull_update.py
@@ -3,15 +3,15 @@ import os
 def main(j, args, params, tags, tasklet):
     params.result = page = args.page
 
-    space_name = args.cmdstr.strip()
-    space = j.core.portal.active.getSpace(space_name)
-    space_path = os.path.abspath(space.model.path)
-
     try:
+        spaces = j.core.portal.active.getSpaces()
+        for space_name in spaces:
+            space = j.core.portal.active.getSpace(space_name, ignore_doc_processor=True)
+            space_path = os.path.abspath(space.model.path)
         j.system.process.execute('cd %s;git pull' % space_path)
         page.addMessage('Pulled and Updated')
     except Exception, error:
-        page.addMessage(error)
+        page.addMessage('Something went wrong with updating one more or more of the spaces')
 
     return params
 
