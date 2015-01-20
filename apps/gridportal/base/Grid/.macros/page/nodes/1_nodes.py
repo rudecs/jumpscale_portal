8e0534f97051a471cf7b776bebff1c93b27c2e5f
Jo De Boeck <deboeck.jo@gmail.com>
2015-01-20 14:23:50 +0200

    remove unused variable

--- apps/gridportal/base/Grid/.macros/page/nodes/1_nodes.py
+++ apps/gridportal/base/Grid/.macros/page/nodes/1_nodes.py
@@ -5,7 +5,6 @@ def main(j, args, params, tags, tasklet):
 
     fieldnames = ['GID', 'Name', 'Node ID', 'IP Address', 'Roles']
     filters = dict()
-    nativequery = None
     for tag, val in args.tags.tags.iteritems():
         if tag in ('gid', ) and val and not val.startswith("$$"):
             filters['gid'] = int(val)
@@ -15,7 +14,7 @@ def main(j, args, params, tags, tasklet):
     namelink = '[%(name)s|/grid/node?id=%(id)s&gid=%(gid)s]'
     fieldvalues = ['gid', namelink, 'id','ipaddr', 'roles']
     fieldids = ['gid', 'name', 'id', 'ipaddr', 'roles']
-    tableid = modifier.addTableForModel('system', 'node', fieldids, fieldnames, fieldvalues, filters, nativequery)
+    tableid = modifier.addTableForModel('system', 'node', fieldids, fieldnames, fieldvalues, filters)
     modifier.addSearchOptions('#%s' % tableid)
 
     params.result = page
