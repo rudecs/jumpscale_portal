e7f59bf8bd8c37abb48e6f1471c014da1e13bfd1
hamdy <hamdy.a.farag@gmail.com>
2015-02-05 13:24:57 +0200

    Moving codegentools to jumpscale_core7

--- lib/portal/__init__.py
+++ lib/portal/__init__.py
@@ -5,7 +5,7 @@ import JumpScale.baselib.key_value_store
 #import JumpScale.baselib.taskletengine
 import JumpScale.baselib.specparser
 
-from . import codegentools
+import JumpScale.baselib.codegentools
 from . import docgenerator
 from . import docpreprocessor
 from . import portal
--- lib/portal/codegentools/__init__.py
+++
@@ -1,4 +0,0 @@
-from JumpScale import j
-from .CodeGenerator import CodeGenerator
-j.base.loader.makeAvailable(j, 'core')
-j.core.codegenerator = CodeGenerator()
