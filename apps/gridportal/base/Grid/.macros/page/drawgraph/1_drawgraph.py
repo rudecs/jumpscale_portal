f0b297ebb13a59e35b8ccdcfb598c21c07ec4ec2
Reem Khamis <khamisr@codescalers.com>
2015-02-24 17:51:40 +0200

    Macro to create grafana dashboards from user's definition + Example

---
+++ apps/gridportal/base/Grid/.macros/page/drawgraph/1_drawgraph.py
@@ -0,0 +1,176 @@
+def main(j, args, params, tags, tasklet):
+    page = args.page
+
+    graphdata = args.cmdstr
+    graphdata = j.core.hrd.get(content=graphdata)
+
+    target = graphdata.getDict('target')
+    cfg = graphdata.getDictFromPrefix('cfg')
+    
+    targetsstr = """
+        {
+            "target": "randomWalk('random walk')",
+            "function":"%(function)s",
+            "column": "value / %(divisor)s",
+            "series": "%(series)s",
+            "query": "",
+            "alias": "%(alias)s",
+            "interval": "%(interval)s"
+        }
+        """ % target
+
+    cfg['target'] = targetsstr
+
+    configuration = """
+ {
+    "title": "Grafana",
+    "tags": [],
+    "style": "light",
+    "timezone": "browser",
+    "editable": false,
+    "rows": [
+        {
+            "title": "%(title)s",
+            "height": "250px",
+            "editable": false,
+            "collapse": false,
+            "collapsable": true,
+            "panels": [
+                {
+                    "editable": false,
+                    "type": "graph",
+                    "x-axis": true,
+                    "y-axis": true,
+                    "scale": 1,
+                    "y_formats": [
+                        "short",
+                        "short"
+                    ],
+                    "grid": {
+                        "max": null,
+                        "min": null,
+                        "leftMax": null,
+                        "rightMax": null,
+                        "leftMin": null,
+                        "rightMin": null,
+                        "threshold1": null,
+                        "threshold2": null,
+                        "threshold1Color": "rgba(216,200,27,0.27)",
+                        "threshold2Color": "rgba(234,112,112,0.22)"
+                    },
+                    "resolution": 100,
+                    "lines": true,
+                    "fill": 1,
+                    "linewidth": 2,
+                    "points": false,
+                    "pointradius": 5,
+                    "bars": false,
+                    "stack": true,
+                    "spyable": true,
+                    "options": false,
+                    "legend": {
+                        "show": true,
+                        "values": false,
+                        "min": false,
+                        "max": false,
+                        "current": false,
+                        "total": false,
+                        "avg": false
+                    },
+                    "interactive": true,
+                    "legend_counts": true,
+                    "timezone": "browser",
+                    "percentage": false,
+                    "zerofill": true,
+                    "nullPointMode": "connected",
+                    "steppedLine": false,
+                    "tooltip": {
+                        "value_type": "cumulative",
+                        "query_as_alias": true
+                    },
+                    "targets": [
+                        %(target)s
+                    ],
+                    "aliasColors": {},
+                    "aliasYAxis": {},
+                    "title": "%(title)s",
+                    "datasource": null,
+                    "renderer": "flot",
+                    "annotate": {
+                        "enable": false
+                    }
+                }
+            ],
+            "notice": false
+        }
+    ],
+    "pulldowns": [
+        {
+            "type": "filtering",
+            "collapse": false,
+            "notice": false,
+            "enable": false
+        },
+        {
+            "type": "annotations",
+            "enable": false
+        }
+    ],
+    "nav": [
+        {
+            "type": "timepicker",
+            "collapse": false,
+            "notice": false,
+            "enable": true,
+            "status": "Stable",
+            "time_options": [
+                "5m",
+                "15m",
+                "1h",
+                "6h",
+                "12h",
+                "24h",
+                "2d",
+                "7d",
+                "30d"
+            ],
+            "refresh_intervals": [
+                "5s",
+                "10s",
+                "30s",
+                "1m",
+                "5m",
+                "15m",
+                "30m",
+                "1h",
+                "2h",
+                "1d"
+            ],
+            "now": true
+        }
+    ],
+    "time": {
+        "from": "now-1h",
+        "to": "now"
+    },
+    "templating": {
+        "list": []
+    },
+    "version": 2
+}
+""" % cfg
+
+    md5 = j.tools.hash.md5_string(configuration)
+    path = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'portals', 'jslib', 'grafana', 'app', 'dashboards', '%s.json' % md5)
+    if not j.system.fs.exists(path):
+        j.system.fs.writeFile(path, configuration)
+
+    page.addHTML("""
+        <iframe width="1000px" height="300px" src="/jslib/grafana/iframe.html#/dashboard/file/%s.json" frameborder="0"></iframe>
+      """% md5)
+    params.result = page
+    return params
+
+
+def match(j, args, params, tags, tasklet):
+    return True
