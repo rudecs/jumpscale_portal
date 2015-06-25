dc962db57456c3ec58959d60c3aedb47fc67ef23
Jo De Boeck <deboeck.jo@gmail.com>
2015-06-25 08:20:37 +0200

    nodebootstrap: use beaker session id instead of getting it out of the cookie

--- apps/gridportal/base/Grid/.macros/wiki/nodebootstrap/5_nodebootstrap.py
+++ apps/gridportal/base/Grid/.macros/wiki/nodebootstrap/5_nodebootstrap.py
@@ -1,17 +1,10 @@
-import datetime
-
-
 def main(j, args, params, tags, tasklet):
     params.merge(args)
     doc = params.doc
 
     ctx = args.get('requestContext')
     host = ctx.env['HTTP_HOST']
-    cookies = ctx.env['HTTP_COOKIE']
-    res = j.codetools.regex.findAll('beaker.session.id=(\w+)', cookies)
-    authKey = "unknown"
-    if res > 0:
-        authKey = res[0].split('=')[1]
+    authKey = ctx.env['beaker.session'].id
 
     url = "http://%s/restmachine/system/cpunode/init?authkey=%s" % (host, authKey)
     out = """{{code: template:text nolinenr theme:neat
