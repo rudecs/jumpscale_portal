96b6d1606ff3176b8877125cf929fbdfc76cba05
Jo De Boeck <deboeck.jo@gmail.com>
2015-07-06 08:06:07 +0200

    Dont attempt to convert from string if type is already correct

--- lib/portal/portal/PortalRest.py
+++ lib/portal/portal/PortalRest.py
@@ -29,14 +29,14 @@ class PortalRest():
                     ctx.start_response('400 Bad Request', [])
                     message = 'Param with name:%s is missing.' % key
                     return False, message
-            elif param['type'] == 'int':
+            elif param['type'] == 'int' and not isinstance(ctx.params[key], int):
                 try:
                     ctx.params[key] = int(ctx.params[key])
                 except ValueError:
                     ctx.start_response('400 Bad Request', [])
                     msg = 'Value of param %s not correct needs to be of type %s' % (key, param['type'])
                     return False, msg
-            elif param['type'] == 'bool':
+            elif param['type'] == 'bool' and not isinstance(ctx.params[key], bool):
                 try:
                     ctx.params[key] = j.basetype.boolean.fromString(ctx.params[key])
                 except ValueError:
