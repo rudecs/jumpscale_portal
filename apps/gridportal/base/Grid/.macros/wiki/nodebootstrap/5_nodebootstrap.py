8cf62a367cf9bd2c3ce5fd1ed0339e3deede7374
Jo De Boeck <deboeck.jo@gmail.com>
2015-07-09 23:17:37 +0200

    Try to get the url scheme from header

--- apps/gridportal/base/Grid/.macros/wiki/nodebootstrap/5_nodebootstrap.py
+++ apps/gridportal/base/Grid/.macros/wiki/nodebootstrap/5_nodebootstrap.py
@@ -4,9 +4,10 @@ def main(j, args, params, tags, tasklet):
 
     ctx = args.get('requestContext')
     host = ctx.env['HTTP_HOST']
+    scheme = ctx.env.get('HTTP_X_FORWARDED_PROTO', 'http')
     authKey = ctx.env['beaker.session'].id
 
-    url = "https://%s/restmachine/system/cpunode/init?authkey=%s" % (host, authKey)
+    url = "%s://%s/restmachine/system/cpunode/init?authkey=%s" % (scheme, host, authKey)
     out = """{{code: template:text nolinenr theme:neat
 curl -L %s | bash
 }}""" % url
