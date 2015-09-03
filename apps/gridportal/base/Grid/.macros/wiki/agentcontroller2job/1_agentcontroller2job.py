5a0349e98d335db6a523acee16d1190957eb4b91
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-09-03 09:39:50 +0200

    WIP: refactor agentcontroller 2 pages

--- apps/gridportal/base/Grid/.macros/wiki/agentcontroller2job/1_agentcontroller2job.py
+++ apps/gridportal/base/Grid/.macros/wiki/agentcontroller2job/1_agentcontroller2job.py
@@ -1,38 +1,63 @@
-import datetime
-import json # pretty printer require native json
+import json
 
-def main(j, args, params, tags, tasklet):    
-    import urllib
 
-    jobid = args.getTag('jobid')
-    if not jobid:
-        out = 'Missing jobresult jobid param "jobid"'
+def prettify(j, s):
+    if isinstance(s, basestring):
+        try:
+            s = json.loads(s)
+        except:
+            pass
+
+    return j.html.escape(json.dumps(s, indent=4, sort_keys=True))
+
+
+def main(j, args, params, tags, tasklet):
+    job_str = args.getTag('job')
+
+    if not job_str:
+        out = 'Missing job param "job"'
         params.result = (out, args.doc)
         return params
 
+    job = json.loads(job_str.decode('base64'))
+
+    job['args'] = prettify(j, job['args'])
+    job['data'] = prettify(j, job['data'])
+
+    jobid = job['id']
+
     clients = j.atyourservice.findServices('jumpscale', 'agentcontroller2_client')
     if not clients:
-        params.result = ('* no agentcontroller2client installed. Use "ays install agentcontroller2_client"', doc)
+        params.result = ('* Agent controller 2 is not available, are you missing the client?', params.doc)
         return params
-
 
     instance = clients[0].instance
     acclient = j.clients.ac.getByInstance(instance)
-    job = acclient.get_by_id(None, None, jobid)
- 
-    if not job:
+    job_ref = acclient.get_by_id(None, None, jobid)
+
+    # Note we are retrieving the job results directly with the client, not from the passed job
+    # object so when you can see changes in result state by refreshing the page.
+    # the job object is maily passed for job data.
+    if not job_ref:
         params.result = ('Job with jobid %s not found' % jobid, args.doc)
         return params
 
-    jobresult = {'id': jobid}
-    results = job.noblock_get_result()
+    jobresult = {
+        'job': job
+    }
+
+    results = job_ref.noblock_get_result()
 
     for result in results:
+        # reget the job result on, but linked with the correct agent to
+        # retrieve the log messages.
         cmd = acclient.get_by_id(result['gid'], result['nid'], jobid)
+
+        result['data'] = prettify(j, result['data'])
         result['msgs'] = cmd.get_msgs()
         result['msgs'].reverse()
         result['starttime'] = j.base.time.epoch2HRDateTime(result['starttime'])
-        result['time'] = result['time']/1000
+        result['time'] = result['time'] / 1000
 
     jobresult['results'] = results
     args.doc.applyTemplate(jobresult)
