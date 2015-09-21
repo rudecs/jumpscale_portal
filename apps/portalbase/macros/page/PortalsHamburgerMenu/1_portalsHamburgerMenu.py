169da39eb6b1358f15880df41ac61305c61d9049
Jo De Boeck <deboeck.jo@gmail.com>
2015-09-21 09:36:31 +0200

    Change structure of data

--- apps/portalbase/macros/page/PortalsHamburgerMenu/1_portalsHamburgerMenu.py
+++ apps/portalbase/macros/page/PortalsHamburgerMenu/1_portalsHamburgerMenu.py
@@ -4,9 +4,13 @@ def main(j, args, params, tags, tasklet):
     page = args.page
     hrd = j.application.instanceconfig
 
-    menulinks = hrd.getDictFromPrefix('instance.navigationlinks')
+    menulinks = hrd.getListFromPrefix('instance.navigationlinks')
     if not menulinks:
-        menulinks = {'Portal': j.core.portal.active.getSpaceLinks(args.requestContext)}
+        spacelinks = j.core.portal.active.getSpaceLinks(args.requestContext)
+        menulinks = []
+        for name, url in spacelinks.iteritems():
+            menulinks.append({'name': name, 'url': url, 'theme': 'dark', 'external': 'false'})
+
     hrdListHTML = j.core.portal.active.templates.render('system/hamburgermenu/structure.html', menulinks=menulinks)
     script = j.core.portal.active.templates.render('system/hamburgermenu/script.js', hrdListHTML=hrdListHTML).replace('\n', '')
     style = j.core.portal.active.templates.render('system/hamburgermenu/style.css')
