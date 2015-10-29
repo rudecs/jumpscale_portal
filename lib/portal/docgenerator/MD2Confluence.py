e707f0e1c0ef5d6fbe64a07390e298fda3f0e233
hamdy <hamdy.farag@codescalers.com>
2015-10-29 19:02:39 +0200

    md2confluence conversion [not yet completed]

--- lib/portal/docgenerator/MD2Confluence.py
+++ lib/portal/docgenerator/MD2Confluence.py
@@ -3,60 +3,274 @@ from JumpScale import j
 
 import re, os, sys
 
-class MD2Confluence():
+class Image(object):
+    def __init__(self, line):
+        self.regex = '\!\[([\w\-_\s]*)\]'
+        self.line = line
+        self._exists = False
+        self._alt = ''
+    
+    def parse(self):
+        if self.line:
+            m = re.search(self.regex, self.line)
+            if m:
+                try:
+                    self._alt = m.group(1)
+                    self._exists = True
+                except IndexError:
+                    pass
+    @property
+    def exists(self):
+        return self._exists
+    
+    @property
+    def alt(self):
+        return self._alt
 
+    @property
+    def wiki(self):
+        return '!%s!' % self.url
+
+    @property
+    def url(self):
+        newline = self.line.split('![%s]' % self.alt)[1]
+        if newline.startswith('('):
+            return newline.split(')')[0][1:]
+
+class URL(object):
+    def __init__(self, line):
+        self.regex = '\[([\w\-_:\/.*\s]*)\]'
+        self.line = line
+        self._exists = False
+        self._alt = ''
+    
+    def parse(self):
+        if self.line:
+            m = re.search(self.regex, self.line)
+            if m and '!' not in self.line:
+                try:
+                    self._alt = m.group(1)
+                    self._exists = True
+                except IndexError:
+                    pass
+
+    @property
+    def exists(self):
+        return self._exists
+    
+    @property
+    def alt(self):
+        return self._alt
+
+    @property
+    def wiki(self):
+        u = '[%s' % self._alt 
+        u +=  '|%s' %  self.url  if self.url else ''
+        u += ']'
+        return u
+
+    @property
+    def url(self):
+        newline = self.line.split('[%s]' % self.alt)[1]
+        if newline.startswith('('):
+            return newline.split(')')[0][1:]
+
+
+class Header(object):
+    def __init__(self, line, **kwargs):
+        self._exists = False
+        self.line  = line
+        self._wiki = ''
+        self._lines = kwargs.get('lines')
+        self._curridx = kwargs.get('currentidx')
+        self._newcontent = kwargs.get('newContent')
+
+    def _remove_previous_line(self):
+        self._newcontent.pop()
+    
+    def parse(self):
+        self._exists = True
+        if self.line.startswith('######'):
+            self._wiki =  'h6. %s' % self.line.replace('######', '')
+        elif self.line.startswith('#####'):
+            self._wiki = 'h5. %s' % self.line.replace('#####', '')
+        elif self.line.startswith('####'):
+            self._wiki =  'h4. %s' % self.line.replace('####', '')
+        elif self.line.startswith('###'):
+            self._wiki =  'h3. %s' % self.line.replace('###', '')
+        elif self.line.startswith('##'):
+            self._wiki =  'h2. %s' % self.line.replace('##', '')
+        elif self.line.startswith('#'):
+            self._wiki =  'h1. %s' % self.line.replace('#', '')
+        elif self.line.startswith('='):
+            self._wiki =  'h1. %s' % self._lines[self._curridx - 1]
+            self._remove_previous_line()
+        elif self.line.startswith('--'):
+            self._wiki =  'h5. %s' % self._lines[self._curridx - 1]
+            self._remove_previous_line()
+        else:
+            self._exists = False
+
+    @property
+    def exists(self):
+        return self._exists
+    
+    @property
+    def wiki(self):
+        return self._wiki
+
+
+class TableElement(object):
+    def __init__(self, line, **kwargs):
+        self.line = line
+        self._exists = False
+        self._header = ''
+        self._row = ''
+        self._lines = kwargs.get('lines')
+        self._curridx = kwargs.get('currentidx')
+        self._wiki = ''
+        self._newcontent = kwargs.get('newContent')
+
+    def _remove_previous_line(self):
+        return self._newcontent.pop()
+
+    def parse(self):
+        if self.line.startswith('|'):
+            self._exists = True
+            if '--' in self.line:
+                previousline = self._remove_previous_line()
+                self._header = previousline.replace('|', '||')
+            else:
+                self._row = self.line
+    
+    @property
+    def exists(self):
+        return self._exists
+    
+    @property
+    def wiki(self):
+        if self._header:
+            return self._header
+        else:
+            return self._row
+
+    @property
+    def url(self):
+        newline = self.line.split('[%s]' % self.alt)[1]
+        if newline.startswith('('):
+            return newline.split(')')[0][1:]
+
+
+class Code(object):
+    def __init__(self, line, **kwargs):
+        self.line = line
+        self._exists = False
+        self._lines = kwargs.get('lines')
+        self._curridx = kwargs.get('currentidx')
+        self._newcontent = kwargs.get('newContent')
+    
+#     def parse(self):
+#         codelines = []
+#         if line.strip() == '```':
+#             for i in range(self._curridx-1, -1, -1):
+#                 if '```' not in self._lines[i].strip():
+#                     codelines.insert(0, self._newcontent.pop())
+                
+    
+    @property
+    def exists(self):
+        return self._exists
+    
+    @property
+    def wiki(self):
+        pass
+    
+    
+
+
+        
+
+class MD2Confluence():
     def convert(self,c):
         # replace all links
         # c = re.sub(r'\[([^\]]+)\]\(([^\]]+)\)', r'[\1|\2]', c)
-        c = re.sub(r'\[(.*?)\]\((.*?)\)', r'[\1|\2]', c)
-
-        # replace bold temporarily
+#         c = re.sub(r'\[(.*?)\]\((.*?)\)', r'[\1|\2]', c)
+# 
+#         # replace bold temporarily
         c = re.sub(r'\*\*(.*?)\*\*', r'bdirkb\1bdirkb', c)
-        # replace italics
+#         # replace italics
         c = re.sub(r'\*(.*?)\*', r'_\1_', c)
-        # replace bold
+#         # replace bold
         c = re.sub(r'bdirkb(.*?)bdirkb', r'*\1*', c)
-
-        # replace inline code
-        c = re.sub(r'`(.*?)`', r'*\1*', c)
+# 
+#         # replace inline code
+#         c = re.sub(r'`(.*?)`', r'*\1*', c)
 
         # print c
         c = c.split('\n')
-
         words = []
-        words.append( ['#','h1.'] )
-        words.append( ['##','h2.'] )
-        words.append( ['###','h3.'] )
-        words.append( ['####','h4.'] )
-        words.append( ['#####','h5.'] )
-        words.append( ['######','h6.'] )
-
         newContent = []
 
-        i = 0
         isCode = 0
         indent = 0
         isQuote = 0
         isList = 0
 
-        for l in c:
-          i += 1
-          if 0 == 0:
-            # print l[:30]
+        for idx, l in enumerate(c):
+            
+             #Table element
+            tableelement = TableElement(l, lines=c, currentidx=idx, newContent=newContent)
+            tableelement.parse()
+            if tableelement.exists:
+                newContent.append(tableelement.wiki)
+                continue
+            
+            # Headers
+            header = Header(l, lines=c, currentidx=idx, newContent=newContent)
+            header.parse()
+            if header.exists:
+                newContent.append(header.wiki)
+                continue
+            
+            #URLs
+            url = URL(l)
+            url.parse()
+            if url.exists:
+                newContent.append(url.wiki)
+                #remove that rendered url from line
+                l = l.replace('[%s]' % url.alt, '').replace('(%s)' % url.url, '')
+                
+
+            #Images
+            img = Image(l)
+            img.parse()
+            if img.exists:
+                newContent.append(img.wiki)
+                #remove that rendered image from line
+                l = l.replace('![%s]' % img.alt, '').replace('(%s)' % img.url, '')
+
+#             code = Code(l, lines=c, currentidx=idx, newContent=newContent)
+#             code.parse()
+#             if code.exists:
+#                 newContent.append(code.wiki)
+
+            #code
+            
+#             # print l[:30]
             k = l
             if l[0:1]=='*':
               isList = 1
             if l == '':
               isList = 0
-
+ 
             if l[0:1] == '>':
               if isQuote==0:
                 k = '{quote}\n'+k[1:]
                 isQuote = 1
               else:
                 k = k[1:]
-
-
+ 
+ 
             if isList == 0:
               pass
               # if isCode == 1:
@@ -78,24 +292,26 @@ class MD2Confluence():
                  k = '***' + l[3:]
                if l[0:2]=='\t*':
                  k = '**' + l[2:]
-
+ 
             for w in words:
               # print l[:len(w[0])]
               if l[:len(w[0])] == w[0]:
                 k = w[1]+l[len(w[0]):]
-
-
+ 
+ 
             if l[0:1] != '>' and isQuote == 1:
               k = '{quote}\n'+k
               isQuote = 0
-
-
+ 
+ 
             if l[0:3] != '| -':
               newContent.append(k)
-
-            # print k
-            i# newContent.append(k)
-
+               
+             
+             
+ 
+            print k
+            #newContent.append(k)
         return '\n'.join(newContent)
 
 
