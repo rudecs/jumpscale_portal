d57aa7d1a231b608c45dc48ac848e1a0f209d46f
Jo De Boeck <deboeck.jo@gmail.com>
2015-12-02 13:53:07 +0200

    Fix typo in jobs grid filter

--- apps/gridportal/base/Grid/.macros/page/jobs/1_jobs.py
+++ apps/gridportal/base/Grid/.macros/page/jobs/1_jobs.py
@@ -44,7 +44,7 @@ def main(j, args, params, tags, tasklet):
 
     fieldnames = ['Create Time', 'Start', 'Stop', 'Category', 'Command', 'Queue', 'State']
     fieldvalues = [makeLink, modifier.makeTimeOnly, modifier.makeTimeOnly, 'category', 'cmd', 'queue', 'state']
-    fieldids = ['timeCreate', 'timeStart', 'timeStop', 'category', 'cmd', 'qeueu', 'state']
+    fieldids = ['timeCreate', 'timeStart', 'timeStop', 'category', 'cmd', 'queue', 'state']
     tableid = modifier.addTableForModel('system', 'job', fieldids, fieldnames, fieldvalues, nativequery=filters)
     modifier.addSearchOptions('#%s' % tableid)
     modifier.addSorting('#%s' % tableid, 0, 'desc')
