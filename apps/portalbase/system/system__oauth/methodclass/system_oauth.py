013a7943dd40c39ab86e0f940422c6ae4cd4090b
Jo De Boeck <deboeck.jo@gmail.com>
2016-02-10 14:58:46 +0200

    OAUTH: improve error handling

--- apps/portalbase/system/system__oauth/methodclass/system_oauth.py
+++ apps/portalbase/system/system__oauth/methodclass/system_oauth.py
@@ -2,6 +2,7 @@ import urllib
 import requests
 
 from JumpScale import j
+from JumpScale.portal.portal import exceptions
 import JumpScale.baselib.redisworker
 try:
     import ujson as json
@@ -53,13 +54,11 @@ class system_oauth(j.code.classGetBase()):
         ctx = kwargs['ctx']
         code = kwargs.get('code')
         if not code:
-            ctx.start_response('403 Not Authorized', [])
-            return 'Not Authorized -- Code is missing'
+            raise exceptions.Forbidden('Not Authorized -- Code is missing')
         
         state = kwargs.get('state')
         if not state:
-            ctx.start_response('403 Not Authorized', [])
-            return 'Not Authorized -- State is missing'
+            return exceptions.Forbidden('Not Authorized -- State is missing')
         
         cache = j.clients.redis.getByInstance('system')
         cache_result = cache.get(state)
@@ -68,9 +67,8 @@ class system_oauth(j.code.classGetBase()):
             unauthorized_redirect_url = '%s?%s' % ('/restmachine/system/oauth/authenticate', urllib.urlencode({'type': j.core.portal.active.force_oauth_instance or 'github'}))
             msg = 'Not Authorized -- Invalid or expired state'
             j.logger.log(msg)
-            ctx.start_response('302 Found', [('Location', unauthorized_redirect_url)])
-            return msg
-        
+            raise exceptions.Redirect(unauthorized_redirect_url)
+
         cache_result = json.loads(cache_result)
         client = j.clients.oauth.get(instance=cache_result['type'])
         payload = {'code': code, 'client_id': client.id, 'client_secret': client.secret, 'redirect_uri': client.redirect_url, 'grant_type':'authorization_code'}
@@ -79,9 +77,8 @@ class system_oauth(j.code.classGetBase()):
         if not result.ok or 'error' in result.json():
             msg = 'Not Authorized -- %s' % result.json()['error']
             j.logger.log(msg)
-            ctx.start_response('403 Not Authorized', [])
-            return msg
-        
+            raise exceptions.Forbidden(msg)
+
         result = result.json()
         access_token = result['access_token']
         params = {'access_token' : access_token}
@@ -102,13 +99,14 @@ class system_oauth(j.code.classGetBase()):
         else:
             u = users[0]
             if email not in u['emails']:
-              ctx.start_response('400 Bad Request', [])
-              return 'User witht the same name already exists'         
+                raise exceptions.BadRequest('User with same name already exists')
 
         session = ctx.env['beaker.session']
         session['user'] = username
         session['email'] = email
-        session['oauth'] = {'authorized':True, 'type':str(cache_result['type']), 'logout_url':client.logout_url}
+        session['oauth'] = {'authorized': True,
+                            'type': str(cache_result['type']),
+                            'logout_url': client.logout_url}
         session.save()
-        
-        ctx.start_response('302 Found', [('Location', str(cache_result['redirect']))])
+
+        raise exceptions.Redirect(str(cache_result['redirect']))
