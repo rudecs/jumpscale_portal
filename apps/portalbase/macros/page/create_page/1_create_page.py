be824c87e0b8511e74d379c54ddbd8f588238ec8
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2016-09-20 16:55:06 +0200

    bug fix openvcloud  #199

--- apps/portalbase/macros/page/create_page/1_create_page.py
+++ apps/portalbase/macros/page/create_page/1_create_page.py
@@ -2,10 +2,8 @@ import os
 import re
 from JumpScale.portal.portal import exceptions
 
-
-
 def errmsg(s):
-    return '<div class="alert alert-danger" role="alert">%s</div>'%(s)
+     return '<div class="alert alert-danger" role="alert">%s</div>'%(s)
 
 def main(j, args, params, tags, tasklet):
     params.result = page = args.page
@@ -20,11 +18,18 @@ def main(j, args, params, tags, tasklet):
         raise exceptions.BadRequest("BadRequest", "text/plain")
 
     page_name = args.paramsExtra.get('page_name')
+
+    space = j.core.portal.active.getSpace(page_space).model
+
     # Creating a new page
     page.addMessage('''<h2>Create a new page in '%s' space</h2>''' % j.html.escape(page_space) )
     if page_name and page_space:
         if not re.search('^[\s\w\d]+$', page_name):
             page.addMessage(errmsg('***ERROR***: The page name should not contain any special characters'))
+            return params
+
+        if page_name in os.listdir(space.path):
+            page.addMessage(errmsg('***ERROR***: The page name already exists'))
             return params
 
         space = j.core.portal.active.getSpace(page_space)
