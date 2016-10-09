6815ed4e7876f967db5f968594d27c40c926f896
Jo De Boeck <deboeck.jo@gmail.com>
2016-10-09 12:13:30 +0200

    Add more info to repoversion macro

--- apps/portalbase/macros/wiki/repoversion/3_repoversion.py
+++ apps/portalbase/macros/wiki/repoversion/3_repoversion.py
@@ -6,7 +6,11 @@ def main(j, args, params, tags, tasklet):
     acl = j.clients.agentcontroller.get()
     jsargs = {'account': account, 'repo': repo, 'provider': provider}
     jobresult = acl.executeJumpscript('jumpscale', 'repoversion', role='master', gid=j.application.whoAmI.gid, args=jsargs)
-    wiki = jobresult['result'][1]
+    result = jobresult['result']
+    link = result['hex']
+    if provider == 'github':
+        link = '[{0}|https://github.com/{1}/{2}/commit/{0}]'.format(result['hex'], account, repo)
+    wiki = "%s: %s (%s) {{ts: %s}}" % (result['version'][0], result['version'][1], link, result['timestamp'])
 
     params.result = (wiki, args.doc)
     return params
