c9dd909f2c0f776c3bfc02203420dbe2afad8ca1
Samar Adel <adels@greenitglobe.com>
2016-10-23 15:00:59 +0200

    Fix cloud space menu links (#389)

--- apps/portalbase/templates/system/hamburgermenu/script.js
+++ apps/portalbase/templates/system/hamburgermenu/script.js
@@ -162,4 +162,9 @@ $(function () {
         checkChangedBrowserSize();
     };
 
+    var shiftWindow = function() {
+      scrollBy(0, -50)
+    };
+    if (location.hash) shiftWindow();
+    window.addEventListener("hashchange", shiftWindow);
 });
