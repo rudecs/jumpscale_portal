aac2f338c9fc7e8b73b56b277155cfaf3a2f0e6b
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2017-01-30 16:03:11 +0200

    adjustments to emailsender actor to make functional (#75)
    temporary as html not supported yet

--- apps/portalbase/system/system__emailsender/methodclass/system_emailsender.py
+++ apps/portalbase/system/system__emailsender/methodclass/system_emailsender.py
@@ -1,6 +1,8 @@
 import smtplib, os
 from JumpScale import j
 import JumpScale.baselib.mailclient
+from JumpScale.portal.portal import exceptions
+
 
 ujson = j.db.serializers.getSerializerType('j')
 
@@ -19,22 +21,20 @@ class system_emailsender(j.code.classGetBase()):
         self._te = {}
         self.actorname = "emailsender"
         self.appname = "system"
-        
 
-    def format(self, obj, format=None):
-        if not format or format not in self.output_format_mapping:
-            format = 'json'
-        output_formatter = self.output_format_mapping[format]
+   def format(self, obj, format=None):		
+        if not format or format not in self.output_format_mapping:		
+            format = 'json'		
+        output_formatter = self.output_format_mapping[format]		
         return output_formatter(obj)
 
-    def send(self, sender_name, sender_email, receiver_email, subject, body, smtp_key, format, *args, **kwargs):
+    def send(self, sender_name, sender_email, receiver_email, subject, body, *args, **kwargs):
         """
         param:sender_name The name of the sender
         param:sender_email The email of the sender
         param:receiver_email The email of the receiver
         param:subject Email subject
         param:body Email body
-        param:format The request & response format of the HTTP request itself
         result 'Success' in case of success, or 'Failure: ERROR_MSG' in case of the error message.
         """
 
@@ -49,7 +49,6 @@ class system_emailsender(j.code.classGetBase()):
 
         kwargs.pop('ctx', None)
 
-        # TODO: configure mail client to use ut
         # smtp_server, smtp_login, smtp_password = j.apps.system.contentmanager.dbmem.cacheGet(smtp_key)
 
         if sender_name:
@@ -58,46 +57,14 @@ class system_emailsender(j.code.classGetBase()):
             sender = sender_email
 
         # This is the same email pattern used in `contact_form` macro
-        # TODO: abstract it in one place
         email_pattern = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$"
         if not j.codetools.regex.match(email_pattern, receiver_email):
-            return 'Error: receiver email is not formatted well.'
+            raise exceptions.BadReques('receiver email is not formatted well.')
         if not j.codetools.regex.match(email_pattern, sender_email):
-            return 'Error: your email is not formatted well.'
+            raise exceptions.BadReques('your email is not formatted well.')
 
         receivers = [receiver_email]
-        kwargs['sender'] = sender_email
-        
-        if kwargs:
-            other_params = []
-            for k, v in list(kwargs.items()):
-                if isinstance(v, list):
-                    v = ', '.join(v)
-                other_params.append('<tr><th>{0}</th><td>{1}</td></tr>'.format(k, v))
-
-            body = body + '<br /><table border=1>{0}</table>'.format(''.join(other_params))
-
-        self.save_emails(sender_name, sender_email, receiver_email, subject, body, *args, **kwargs)
         j.clients.email.send(receivers, sender, subject, body)
 
         return 'Success'
 
-    def save_emails(self, sender_name, sender_email, receiver_email, subject, body, *args, **kwargs):
-        system_path = j.core.portal.active.getSpace('system').model.path
-        emails_file = os.path.join(system_path, '.space', 'emails.json')
-        try:
-            emails = j.db.serializers.ujson.loads(open(emails_file).read())
-        except IOError: # File doesn't exist yet
-            emails = []
-
-        emails.append({
-            'sender_name': sender_name,
-            'sender_email': sender_email,
-            'receiver_email': receiver_email,
-            'subject': subject,
-            'body': body,
-            'args': args,
-            'other_data': kwargs
-        })
-
-        open(emails_file, 'w').write(j.db.serializers.ujson.dumps(emails))
--- apps/portalbase/system/system__emailsender/specs/actor.spec
+++ apps/portalbase/system/system__emailsender/specs/actor.spec
@@ -3,10 +3,9 @@
     Email sender
     """
     method:send @tags: noauth
-        var:sender_name str,, Sender full name
+        var:sender_name str,, Sender full name @optional
         var:sender_email str,, Sender email
         var:receiver_email str,, Receiver email.
         var:subject str,, Email subject
         var:body str,, Email body
-        var:smtp_key str,, Email body
         result:str
