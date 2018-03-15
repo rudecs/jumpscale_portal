3a65a37124733ba0699328b47dacda9c29482219
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2018-03-15 13:47:57 +0200

    Adds rows to textarea (#121)
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- lib/portal/docgenerator/popup.py
+++ lib/portal/docgenerator/popup.py
@@ -51,14 +51,14 @@ class Popup(object):
         content = template.render(value=value, name=name)
         self.widgets.append(content)
 
-    def addTextArea(self, label, name, required=False, placeholder='', value=''):
+    def addTextArea(self, label, name, required=False, placeholder='', value='', rows=5):
         template = self.jinja.from_string('''
             <div class="form-group">
                 <label class="line-height" for="${name}">${label}</label>
-                <textarea rows="10" class="form-control" name="${name}" {% if required %}required{% endif %} placeholder="${placeholder}">${value}</textarea>
+                <textarea class="form-control" rows="${rows}" name="${name}" {% if required %}required{% endif %} placeholder="${placeholder}">${value}</textarea>
               </div>
         ''')
-        content = template.render(label=label, name=name, required=required, value=escape_html(value), placeholder=placeholder)
+        content = template.render(label=label, name=name, required=required, value=escape_html(value), placeholder=placeholder, rows=rows)
         self.widgets.append(content)
 
     def addNumber(self, label, name, required=False):
